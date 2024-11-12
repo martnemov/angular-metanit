3e324560090178a40c4dc324d18f2f347074c0e7
Mart Nemov <martnemov@MacBook-Air-Mart.local>
2024-11-12 10:49:27 +0500

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
-import {Component} from "@angular/core";
-import {FormsModule} from "@angular/forms";
-import {DataService} from "./data.service";
-import {DataComponent} from "./data.component";
-import {LogService} from "./log.service";
-
-class User {
-    constructor(
-        public name: string,
-        public age: number,
-        public company: string
-    ) {
-    }
-}
+import { Component, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { DataService } from "./data.service";
+import { DataComponent } from "./data.component";
+import { LogService } from "./log.service";
+import { HttpClient } from "@angular/common/http";
+import { User } from "./user";
 
 @Component({
     selector: "my-app",
@@ -20,45 +13,22 @@ class User {
     providers: [DataService, LogService],
     template: `
         <div>
-            <p>
-                <label>User name</label><br>
-                <input type="text" [(ngModel)]="newUser.name"/>
-            </p>
-            <p>
-                <label>Age</label><br>
-                <input type="number" name="age" [(ngModel)]="newUser.age"/>
-            </p>
-            <p>
-                <label>Company</label><br>
-                <select name="text" [(ngModel)]="newUser.company">
-                    @for (comp of companies; track $index) {
-                        <option [value]="comp">
-                            {{ comp }}
-                        </option>
-                    }
-                </select>
-            </p>
-            <button (click)="addUser()">Add user</button>
-        </div>
-        <div>
-            <h3>List of users</h3>
-            <ul>
-                @for (user of users; track $index) {
-                    <li>{{ user.name }} {{ user.company }} - {{ user.age }}</li>
-                }
-            </ul>
+            <p>Имя пользователя: {{user?.name}}</p>
+            <p>Возраст пользователя: {{user?.age}}</p>
         </div>
     `,
     styles: [`h1 {
         color: black;
     }`]
 })
-export class AppComponent {
-    newUser = new User("", 18 , "");
-    users = [];
-    companies = ['Apple', 'Google', 'Amazon'];
+export class AppComponent implements OnInit {
+    user: User | undefined;
 
-    addUser() {
-        this.users.push({...this.newUser});
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+        this.http.get("assets/data.json").subscribe({
+            next: (data: any) => this.user = new User(data.name, data.age)
+        });
     }
 }
